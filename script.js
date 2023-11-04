9fc2156e0499fc964372ebe7e9ca361b39dfa449
Prashant Zanzan <139844406+prashantzanzan25@users.noreply.github.com>
2023-11-04 11:04:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 //your code here
 
+// Get references to the elements in your HTML
+const app = document.getElementById("app");
+const button = document.querySelector("button");
+
+// Add a click event listener to the button
+button.addEventListener("click", swapTheme);
+
+function swapTheme() {
+    if (app.classList.contains("night")) {
+        app.classList.remove("night");
+        app.classList.add("day");
+        button.textContent = "Switch to Night Theme";
+    } else {
+        app.classList.remove("day");
+        app.classList.add("night");
+        button.textContent = "Switch to Day Theme";
+    }
+}
